9f09ac89f4b1ff9850b88d5dea36172027026448
Mathyx <Dino_Madach4@hotmail.com>
2015-10-14 17:48:56 +0200

    Added scripts to controll the menu functionallity

---
+++ space_concept/Assets/Scripts/MenuScripts/LoadMenuController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadMenuController : MonoBehaviour {
+    public void Button_LoadScene(string f_sceneName)
+    {
+        Application.LoadLevel(f_sceneName);
+    }
+    public void Button_LoadScene(int f_sceneIndex)
+    {
+        Application.LoadLevel(f_sceneIndex);
+    }
+
+    public void Button_LoadSaveGame(string f_saveGameName) {
+        //DO SOMETHING
+    }
+}
---
+++ space_concept/Assets/Scripts/MenuScripts/MainMenuController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenuController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+    public void Button_LoadScene(string f_sceneName){
+        Application.LoadLevel(f_sceneName);
+    }
+    public void Button_LoadScene(int f_sceneIndex){
+        Application.LoadLevel(f_sceneIndex);
+    }
+
+    public void Button_Quit() {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+
+}
