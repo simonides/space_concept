5122dfb930ebc6602943151aaeb5b4953982dfe9
Simon Dimitriadis <pumba6@gmx.at>
2015-11-11 01:27:43 +0100

    rudimental eventlist created, events can be added at runtime, the list scrolls. Events are not using the item pool yet and to show it they have to replace the planet menu.

---
+++ space_concept/Assets/Scripts/GameElements/Menu/EventButton.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+
+public class EventButton : MonoBehaviour {
+
+    public Button button;
+    public Text nameLabel;
+    public Image icon;
+    public Text typeLabel;
+    public Text rarityLabel;
+    public GameObject championIcon;
+}
