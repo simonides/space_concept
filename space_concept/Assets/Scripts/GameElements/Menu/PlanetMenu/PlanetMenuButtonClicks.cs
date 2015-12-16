aacb4ea1aee6a844bff16ebae6e2186bf381d3fa
Simon Dimitriadis <pumba6@gmx.at>
2015-12-16 22:51:01 +0100

    planet menu and send ship menu changed to new assets

---
+++ space_concept/Assets/Scripts/GameElements/Menu/PlanetMenu/PlanetMenuButtonClicks.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlanetMenuButtonClicks : MonoBehaviour {
+
+
+    public void UpgradeFactory()
+    {
+
+    }
+
+    public void UpgradeHangar()
+    {
+
+    }
+
+    public void SendShips()
+    {
+
+    }
+
+    public void CloseMenu()
+    {
+
+    }
+}
