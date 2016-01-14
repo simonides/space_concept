40b9ce60292f92e902b103987da521f3821e46be
jakobmaier <jakobmaier42@gmail.com>
2016-01-14 18:51:02 +0100

    Added AI scripts and GO

---
+++ space_concept/Assets/Scripts/Entities/AiPlayer.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class AiPlayer : PlayerData {
+    public AiPlayer() {
+        IsHumanPlayer = false;
+    }
+
+    public abstract void PerformNextMovement();     
+}
---
+++ space_concept/Assets/Scripts/Entities/BasicAI.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class BasicAI : AiPlayer {
+
+    public override void PerformNextMovement() {
+        throw new NotImplementedException();
+    }
+}
