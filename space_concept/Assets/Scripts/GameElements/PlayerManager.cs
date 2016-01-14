19d6b97368cdf6498f3c6a5ca748c32296ceb01a
jakobmaier <jakobmaier42@gmail.com>
2016-01-14 19:14:23 +0100

    Renamed classes, gos and changed scene for AI stuff

---
+++ space_concept/Assets/Scripts/GameElements/PlayerManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class PlayerManager: MonoBehaviour {
+
+    public PlayerData HumanPlayer { get; private set; }
+    public List<AiPlayer> AiPlayers { get; private set; }
+
+    void Start() {
+        AiPlayers = new List<AiPlayer>();
+        InitEventSubscriptions();
+    }
+
+
+    public void InitMapWithPlayers(int playerCount) {
+
+    }
+
+
+    void InitEventSubscriptions() {
+        MessageHub.Subscribe<NextDayEvent>((NextDayEvent evt) => { PerformAiMovements(); });
+    }
+    
+}
