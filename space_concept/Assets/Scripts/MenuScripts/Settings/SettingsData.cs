9e81d366c06708a5a561dbb2a198d7cb6257fc0a
Mathyx <Dino_Madach4@hotmail.com>
2016-01-15 22:56:59 +0100

    fog of war setting is now saved and loadable and can be accessed over settingsController

--- space_concept/Assets/Scripts/MenuScripts/Settings/SettingsData.cs
+++ space_concept/Assets/Scripts/MenuScripts/Settings/SettingsData.cs
@@ -5,6 +5,7 @@ using System.Xml.Serialization;
 [System.Serializable]
 public class SettingsData  {
 
+    public bool fogOfWar = true;
     public float masterVolume = 100.0f;
     public float musicVolume = 100.0f;
     public float sfxVolume = 100.0f;
