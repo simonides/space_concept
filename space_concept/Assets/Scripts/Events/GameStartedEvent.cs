70754daad27ceafe2ec355b88a07e1ea458f0bea
jakobmaier <jakobmaier42@gmail.com>
2016-01-15 22:14:39 +0100

    DayIndicator is initialised correctly; planet outline is scaled up for small planets

---
+++ space_concept/Assets/Scripts/Events/GameStartedEvent.cs
@@ -0,0 +1,13 @@
+﻿//using UnityEngine;
+//using System.Collections;
+//using TinyMessenger;
+
+//public class GameStartedEvent : ITinyMessage {
+//    public object Sender { get; private set; }
+//    public int FirstDay { get; private set; }       // If the game has been loaded from a save game, this can be >1
+
+//    public GameStartedEvent(object sender, int firstDay) {
+//        Sender = sender;
+//        FirstDay = firstDay;
+//    }
+//}
