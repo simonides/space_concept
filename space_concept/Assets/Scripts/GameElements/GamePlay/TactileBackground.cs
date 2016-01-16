0d7513937299874c6da26bd26548f8707cb47921
jakobmaier <jakobmaier42@gmail.com>
2016-01-16 10:41:30 +0100

    Game scene modified

--- space_concept/Assets/Scripts/GameElements/GamePlay/TactileBackground.cs
+++ space_concept/Assets/Scripts/GameElements/GamePlay/TactileBackground.cs
@@ -7,9 +7,12 @@ public class TactileBackground : MonoBehaviour {
     public GameObject tactileBackgroundPrefab;
     public float tactileBackgroundWith;
 
-    // ****  ATTACHED OBJECTS   **** //
+    public float maxAlpha = 0.7f;
+    public float fadeSpeed = 2f;
     // ****                     **** //
 
+    //public bool should
+    public GameObject backgroundHolder;
 
     public Rect bounds { get; private set; }     // Size/bounds of the cosmos
 
@@ -26,28 +29,32 @@ public class TactileBackground : MonoBehaviour {
     }
 
     void DestroyChildren() {
-        var children = new List<GameObject>();
-        foreach (Transform child in transform) {
-            children.Add(child.gameObject);
-        }
-        children.ForEach(child => Destroy(child));
+        Destroy(backgroundHolder);
+        //var children = new List<GameObject>();
+        //foreach (Transform child in transform) {
+        //    children.Add(child.gameObject);
+        //}
+        //children.ForEach(child => Destroy(child));
     }
 
     void SetupTactileBakground() {
+        backgroundHolder = new GameObject();
+        backgroundHolder.name = "BackgroundHolder";
+        backgroundHolder.transform.SetParent(this.transform);
         Vector2 pos = new Vector2(0,0);
-        for (pos.x = -tactileBackgroundWith; pos.x > bounds.xMin; pos.x -= tactileBackgroundWith) {
+        for (pos.x = -tactileBackgroundWith; pos.x > bounds.xMin- tactileBackgroundWith; pos.x -= tactileBackgroundWith) {
             SetupTactileBakgroundColumn(pos);
         }
-        for (pos.x = 0; pos.x < bounds.xMax; pos.x += tactileBackgroundWith) {
+        for (pos.x = 0; pos.x < bounds.xMax+ tactileBackgroundWith; pos.x += tactileBackgroundWith) {
             SetupTactileBakgroundColumn(pos);
         }
         
     }
     void SetupTactileBakgroundColumn(Vector2 pos) {
-        for (pos.y = -tactileBackgroundWith; pos.y > bounds.yMin; pos.y -= tactileBackgroundWith) {
+        for (pos.y = -tactileBackgroundWith; pos.y > bounds.yMin-tactileBackgroundWith; pos.y -= tactileBackgroundWith) {
             AddTactileBackgroundSnippet(pos);
         }
-        for (pos.y = 0; pos.y < bounds.yMax; pos.y += tactileBackgroundWith) {
+        for (pos.y = 0; pos.y < bounds.yMax+ tactileBackgroundWith; pos.y += tactileBackgroundWith) {
             AddTactileBackgroundSnippet(pos);
         }
     }
@@ -55,15 +62,17 @@ public class TactileBackground : MonoBehaviour {
 
     void AddTactileBackgroundSnippet(Vector2 position) {
         var bgSnippet = GameObject.Instantiate(tactileBackgroundPrefab) as GameObject;
-        bgSnippet.transform.SetParent(this.transform);
+        bgSnippet.transform.SetParent(backgroundHolder.transform);
         bgSnippet.transform.localPosition = position;
     }
                                                  // Use this for initialization
     void Start () {
-	
+        //MessageHub.Subscribe<TactileBackgroundStateEvent>()
 	}
 	
 
-
+    void Update() {
+        //this.gameObject.SetActive(true);
+    }
 
 }
