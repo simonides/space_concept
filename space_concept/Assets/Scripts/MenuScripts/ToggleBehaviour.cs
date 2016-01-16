ffa686971b6bdfebcb9584edd52b2422642d5f2a
Mathyx <Dino_Madach4@hotmail.com>
2016-01-16 17:49:57 +0100

    added toggle animation

---
+++ space_concept/Assets/Scripts/MenuScripts/ToggleBehaviour.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ToggleBehaviour : MonoBehaviour {
+   
+    
+    private UnityEngine.UI.Toggle myToggle;
+    private Animator toggleAnimator;
+
+
+	// Use this for initialization
+	void Awake () {
+        myToggle = gameObject.GetComponent<UnityEngine.UI.Toggle>();
+        toggleAnimator = gameObject.GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void ToggleSwitched()
+    {
+
+        //slide from left to right
+        toggleAnimator.SetBool("isOn", myToggle.isOn);
+    }
+}
