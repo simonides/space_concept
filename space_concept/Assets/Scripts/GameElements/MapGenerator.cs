bc689dada3892c00fec9df55ab40214619cdf248
jakobmaier <jakobmaier42@gmail.com>
2016-01-16 23:01:27 +0100

    Corrected planet sizes

--- space_concept/Assets/Scripts/GameElements/MapGenerator.cs
+++ space_concept/Assets/Scripts/GameElements/MapGenerator.cs
@@ -9,37 +9,39 @@ public class MapGenerator {
 
     static public SpaceData GenerateDefaultMap() {
         SpaceData spaceData = new SpaceData();
-        // pos, diameter, ships, hangar, factory, isStartPlanet
-        spaceData.AddPlanet(new PlanetData(new Vector2(0, 0), 60, 100, 150, 15, true));
-        spaceData.AddPlanet(new PlanetData(new Vector2(1500, 1000), 60, 100, 150, 15, true));
-        spaceData.AddPlanet(new PlanetData(new Vector2(1500, 0), 60, 100, 150, 15, true));
-        spaceData.AddPlanet(new PlanetData(new Vector2(0, 1000), 60, 100, 150, 15, true));
+
         
-        spaceData.AddPlanet(new PlanetData(new Vector2(600, 420), 220, 2200, rnd(250, 380), rnd(150, 250), false));
-        spaceData.AddPlanet(new PlanetData(new Vector2(900, 580), 220, 2200, rnd(250, 380), rnd(150, 250), false));
-        spaceData.AddPlanet(new PlanetData(new Vector2(750, 500), 50, 0, rnd(15, 25), rnd(5, 15), false));
+        // pos, diameter, ships, hangar, factory, isStartPlanet
+        spaceData.AddPlanet(new PlanetData(new Vector2(0, 0), 18, 100, 150, 15, true));
+        spaceData.AddPlanet(new PlanetData(new Vector2(1500, 1000), 18, 100, 150, 15, true));
+        spaceData.AddPlanet(new PlanetData(new Vector2(1500, 0), 18, 100, 150, 15, true));
+        spaceData.AddPlanet(new PlanetData(new Vector2(0, 1000), 18, 100, 150, 15, true));
+        
+        spaceData.AddPlanet(new PlanetData(new Vector2(600, 420), 66, 2200, rnd(250, 380), rnd(150, 250), false));
+        spaceData.AddPlanet(new PlanetData(new Vector2(900, 580), 66, 2200, rnd(250, 380), rnd(150, 250), false));
+        spaceData.AddPlanet(new PlanetData(new Vector2(750, 500), 15, 0, rnd(15, 25), rnd(5, 15), false));
 
-        spaceData.AddPlanet(new PlanetData(new Vector2(640, 550), 40, rnd(1500, 2500), rnd(10, 40), 0, false));
-        spaceData.AddPlanet(new PlanetData(new Vector2(860, 450), 40, rnd(1500, 2500), rnd(10, 40), 0, false));
+        spaceData.AddPlanet(new PlanetData(new Vector2(640, 550), 12, rnd(1500, 2500), rnd(10, 40), 0, false));
+        spaceData.AddPlanet(new PlanetData(new Vector2(860, 450), 12, rnd(1500, 2500), rnd(10, 40), 0, false));
 
-        spaceData.AddPlanet(new PlanetData(new Vector2(80, 200), 80, rnd(40, 60), rnd(110, 140), rnd(15, 20), false));
-        spaceData.AddPlanet(new PlanetData(new Vector2(300, 80), 100, rnd(40, 60), rnd(170, 200), rnd(10, 15), false));
+        spaceData.AddPlanet(new PlanetData(new Vector2(80, 200), 24, rnd(40, 60), rnd(110, 140), rnd(15, 20), false));
+        spaceData.AddPlanet(new PlanetData(new Vector2(300, 80), 30, rnd(40, 60), rnd(170, 200), rnd(10, 15), false));
 
-        spaceData.AddPlanet(new PlanetData(new Vector2(80, 800), 80, rnd(40, 60), rnd(110, 140), rnd(15, 20), false));
-        spaceData.AddPlanet(new PlanetData(new Vector2(300, 920), 100, rnd(40, 60), rnd(170, 200), rnd(10, 15), false));
+        spaceData.AddPlanet(new PlanetData(new Vector2(80, 800), 24, rnd(40, 60), rnd(110, 140), rnd(15, 20), false));
+        spaceData.AddPlanet(new PlanetData(new Vector2(300, 920), 30, rnd(40, 60), rnd(170, 200), rnd(10, 15), false));
 
-        spaceData.AddPlanet(new PlanetData(new Vector2(1420, 200), 80, rnd(40, 60), rnd(110, 140), rnd(15, 20), false));
-        spaceData.AddPlanet(new PlanetData(new Vector2(1200, 80), 100, rnd(40, 60), rnd(170, 200), rnd(10, 15), false));
+        spaceData.AddPlanet(new PlanetData(new Vector2(1420, 200), 24, rnd(40, 60), rnd(110, 140), rnd(15, 20), false));
+        spaceData.AddPlanet(new PlanetData(new Vector2(1200, 80), 30, rnd(40, 60), rnd(170, 200), rnd(10, 15), false));
 
-        spaceData.AddPlanet(new PlanetData(new Vector2(1420, 800), 80, rnd(40, 60), rnd(110, 140), rnd(15, 20), false));
-        spaceData.AddPlanet(new PlanetData(new Vector2(1200, 902), 100, rnd(40, 60), rnd(170, 200), rnd(10, 15), false));
+        spaceData.AddPlanet(new PlanetData(new Vector2(1420, 800), 24, rnd(40, 60), rnd(110, 140), rnd(15, 20), false));
+        spaceData.AddPlanet(new PlanetData(new Vector2(1200, 902), 30, rnd(40, 60), rnd(170, 200), rnd(10, 15), false));
 
 
-        spaceData.AddPlanet(new PlanetData(new Vector2(250, 500), 120, rnd(180, 250), 180, 45, false));
-        spaceData.AddPlanet(new PlanetData(new Vector2(1250, 500), 120, rnd(180, 250), 180, 45, false));
+        spaceData.AddPlanet(new PlanetData(new Vector2(250, 500), 36, rnd(180, 250), 180, 45, false));
+        spaceData.AddPlanet(new PlanetData(new Vector2(1250, 500), 36, rnd(180, 250), 180, 45, false));
 
-        spaceData.AddPlanet(new PlanetData(new Vector2(750, 50), 140, rnd(200, 350), 250, 65, false));
-        spaceData.AddPlanet(new PlanetData(new Vector2(750, 950), 140, rnd(200, 350), 250, 65, false));
+        spaceData.AddPlanet(new PlanetData(new Vector2(750, 50), 42, rnd(200, 350), 250, 65, false));
+        spaceData.AddPlanet(new PlanetData(new Vector2(750, 950), 42, rnd(200, 350), 250, 65, false));
         
         return spaceData;
     }
@@ -53,7 +55,7 @@ public class MapGenerator {
 
         PlanetData planet;
 
-        float minPlanetDistance = 150f;
+        float minPlanetDistance = 70;
 
         float spacePerPlanet = 500;
         float spaceArea = planetCount * spacePerPlanet;
@@ -80,7 +82,7 @@ public class MapGenerator {
             int hangarSize = Random.Range(30, 500);
             int ships = (int)((float)hangarSize * Random.Range(0.05f, 0.4f));
             int factorySpeed = System.Math.Min((int)((float)hangarSize * Random.Range(0.2f, 0.45f)), 100);
-            planet = new PlanetData(new Vector2(newPosition.x, newPosition.y), Random.Range(40f, 150f), ships, hangarSize, factorySpeed, true);
+            planet = new PlanetData(new Vector2(newPosition.x, newPosition.y), Random.Range(15, 58f), ships, hangarSize, factorySpeed, true);
             spaceData.AddPlanet(planet);            
         }
 
