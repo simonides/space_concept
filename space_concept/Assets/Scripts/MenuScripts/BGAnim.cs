3f94b72b531599b4c7d9b5706e4d9d37e29b9beb
Mathyx <Dino_Madach4@hotmail.com>
2016-01-18 22:04:21 +0100

    new Background animation

--- space_concept/Assets/Scripts/MenuScripts/BGAnim.cs
+++ space_concept/Assets/Scripts/MenuScripts/BGAnim.cs
@@ -1,12 +1,36 @@
 ﻿using UnityEngine;
 using System.Collections;
-
-public class BGAnim : MonoBehaviour {
+using Custom.Base;
+public class BGAnim : SingletonBase<BGAnim> {
     [Header("Anim sequence, 1, 1, 2, 1, 3")] 
     public GameObject[] backgrounds;
-   
+
+    public GameObject[] movingParts;
+    public float speed;
+    private float movePoint;
+    private float halfSizeOfSprite;
+    Vector3 moveingSpeed;
+    Vector3 movingPartDistance;
     void Awake(){
-    
+        base.Awake(this);
+        movingParts = new GameObject[5];
+        movingParts[0] = Instantiate(backgrounds[0]);
+        movingParts[1] = Instantiate(backgrounds[0]);
+        movingParts[2] = Instantiate(backgrounds[1]);
+        movingParts[3] = Instantiate(backgrounds[0]);
+        movingParts[4] = Instantiate(backgrounds[2]);
+
+        movePoint = transform.position.x - 200;
+        halfSizeOfSprite = backgrounds[0].GetComponent<SpriteRenderer>().sprite.bounds.size.x *backgrounds[0].transform.localScale.x;
+        movingPartDistance = new Vector3(halfSizeOfSprite, 0, 0);
+        moveingSpeed = Vector3.left;
+
+        for (int i = 0; i < 5; i++) { 
+            movingParts[i].transform.SetParent(transform);
+            movingParts[i].transform.position = transform.position + Vector3.right * (halfSizeOfSprite * i);
+            movingParts[i].SetActive(true);
+        }
+       
     }
 
 	// Use this for initialization
@@ -16,6 +40,31 @@ public class BGAnim : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+        
+        for(int i = 0 ; i < 5; i++)
+        {
+            movingParts[i].transform.position += moveingSpeed * speed * Time.deltaTime;
+            if (movePoint > movingParts[i].transform.position.x)
+            {
+                movingParts[i].transform.position = movingParts[(i + 4) % 5].transform.position + movingPartDistance;
+            }
+        }
+
 	}
+
+    public void DestroyThis()
+    {
+        Debug.Log("Destroyed Menu Backgroud Animation");
+        Destroy(gameObject);
+    }
+
+    //destroys this singleton instance if one exist, otherwise does nothing
+    public static void TryDestroySingleton()
+    {
+        Debug.Log("Try to Destroy Menu Background Animation");
+        if (BGAnim.InstanceExists())
+        {
+            Destroy(BGAnim.GetInstance().gameObject);
+        }
+    }
 }
